d8d91ea1ed235a6f4691e0d6f9b2cc5b88e60261
hori <vmflskgo10@naver.com>
2024-09-04 22:38:43 +0900

    add first goal 1-3

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)  # 게시글 제목
+    content = models.TextField()  # 게시글 내용
+    created_at = models.DateTimeField(default=timezone.now)  # 작성 시간
+
+    def __str__(self):
+        return self.title  # 게시글 제목을 문자열로 반환
