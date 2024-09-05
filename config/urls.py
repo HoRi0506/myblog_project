9d874a7f889c1ccffa70801eece87ebfb983b27c
hori <vmflskgo10@naver.com>
2024-09-05 23:28:45 +0900

    add first goal 1-4

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # 'blog.urls'를 기본 URL로 설정
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
